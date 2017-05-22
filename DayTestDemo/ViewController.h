37bc1e5607c537df20deec81cb13fb90144206f0
_haha_ <zerogo8080@163.com>
2017-05-22 21:24:31 +0800

    xxx

---
+++ DayTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DayTestDemo
+//
+//  Created by Yazi on 2017/5/22.
+//  Copyright © 2017年 Yazi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DayTestDemoTests/DayTestDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  DayTestDemoTests.m
+//  DayTestDemoTests
+//
+//  Created by Yazi on 2017/5/22.
+//  Copyright © 2017年 Yazi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DayTestDemoTests : XCTestCase
+
+@end
+
+@implementation DayTestDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
